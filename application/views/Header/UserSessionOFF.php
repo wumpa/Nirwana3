88c6389d2b296c4f6f75d88e0bb5c1d4d0c2775c
Yusuf Anugrah Putra Aditama <yapaditama@gmail.com>
2015-06-20 11:49:47 +0700

    Applied new CSS
    + profile,logout,cart item

--- application/views/Header/UserSessionOFF.php
+++ application/views/Header/UserSessionOFF.php
@@ -1,7 +1,14 @@
 <?php
 	echo "
+	<div id='register'>
+		<a href='"; echo base_url(); echo "register'>Sign Up</a>
+	</div>
+	<div id='separator'>
+		<a></a>
+	</div>
 	<div id='login'>
 		<a href='"; echo base_url(); echo "login'>Sign In</a>
 	</div>
+	
 	";
 ?>
--- application/views/Header/UserSessionON.php
+++ application/views/Header/UserSessionON.php
@@ -12,8 +12,30 @@
 	}
 
 	echo "
-	<div id='login'>
-		<a href='"; echo base_url(); echo "logout'>"; echo $this->session->userdata('username').' - '.$info; echo"</a>
+	<div id='cart'>
+		<ul id='cart-nav'>
+			<li><a href='"; echo base_url(); echo "'>"; echo $info; echo"</a>
+					<ul>
+						<li><a>View</a></li>
+						<li><a href='"; echo base_url(); echo "logout'>Clear</a></li>
+					</ul>
+			</li>
+		</ul>
 	</div>
+	<div id='separator2'>
+		<a></a>
+	</div>
+	<div id='profile'>
+		<ul id='prof-nav'>
+			<li><a href='"; echo base_url(); echo "'>"; echo $this->session->userdata('username'); echo"</a>
+				<ul>
+					<li><a>Profile</a></li>
+					<li><a href='"; echo base_url(); echo "logout'>Logout</a></li>
+				</ul>
+			</li>
+		</ul>
+	</div>
+
+
 	";
 ?>
