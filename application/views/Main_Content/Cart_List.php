9d77071f28c250b63a6ba8d9d1ec19acbef08010
hikoza9 <Hifzinistique@gmail.com>
2015-06-23 04:51:09 +0700

    add cart list

---
+++ application/views/Main_Content/Cart_List.php
@@ -0,0 +1,13 @@
+<div>
+	<h2>Cart List</h2>
+		<div>
+			<a href="">Merek : </a>
+			<a href="">Tipe : </a>
+			<a href="">Ukuran : </a>
+			<a href="">Warna : </a>
+			<a href="">Harga : </a>
+		</div>
+		<div>
+			<a href=""><img src=""></a>
+		</div>
+</div>
