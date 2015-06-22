6b6614a6e3ab5c360eea8b65ddfa3f455b5eec71
hikoza9 <Hifzinistique@gmail.com>
2015-06-22 23:22:19 +0700

    Create Registration.php

---
+++ application/views/Main_Content/Registration.php
@@ -0,0 +1,14 @@
+<div class="regist">
+	<h2>Sign Up</h2>
+	<form>
+			<label>Username</label>
+			<input type="text" name="username" />
+			<label>Password</label>
+			<input type="password" name="password" />
+			<label>Email</label>
+			<input type="text" name="email" />
+
+			<button type="submit">Sign Up</button>
+		
+	</form>	
+</div>
